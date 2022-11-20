4fc44d422dea0b86c77a2dd9eaf18d842fa12f7c
Füredi Dániel <daniel.furedi03@gmail.com>
2022-11-20 18:40:48 +0100

    all tasks are done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let results = document.getElementById('result-container');
 function displayData(list) {
     let ul = document.createElement('ul');
     ul.style.paddingTop = '20px';
@@ -14,10 +15,10 @@ function displayData(list) {
         li.appendChild(p);
         li.appendChild(q);
         ul.appendChild(li);
-        console.log(quote.id);
     });
     document.getElementById('result-container').appendChild(ul);
 }
+// first task
 document.getElementById('first-task-button').addEventListener('click', () => {
     let taskList = {};
     fetch('./quotes.json')
@@ -25,5 +26,58 @@ document.getElementById('first-task-button').addEventListener('click', () => {
         .then((data) => {
         taskList.quoteList = data.quotes.sort((a, b) => (a.author.toLowerCase() > b.author.toLowerCase()) ? 1 : (b.author.toLowerCase() > a.author.toLowerCase() ? -1 : 0));
         displayData(taskList);
+    });
+});
+// second task
+document.getElementById('second-task-button').addEventListener('click', () => {
+    document.getElementById('result-container').innerHTML = '';
+    let quotesListString = [];
+    fetch('./quotes.json')
+        .then(res => res.json())
+        .then(data => {
+        data.quotes.forEach((e) => {
+            quotesListString.push(e.quote);
+        });
+    })
+        .then(function () {
+        let ol = document.createElement('ol');
+        results.appendChild(ol);
+        ol.style.paddingTop = '20px';
+        quotesListString.forEach(s => {
+            let li = document.createElement('li');
+            let tempString = s.replace(/\bthe\b/g, '<span style="font-weight:bold;">the</span>');
+            let tempString2 = tempString.replace(/\bThe\b/g, '<span style="font-weight:bold;">The</span>');
+            li.innerHTML = tempString2;
+            ol.appendChild(li);
+        });
+    });
+});
+// third task
+document.getElementById('third-task-button').addEventListener('click', () => {
+    results.innerHTML = '';
+    let QuoteListInts = [];
+    fetch('./quotes.json')
+        .then(res => res.json())
+        .then(data => {
+        data.quotes.forEach((q) => {
+            QuoteListInts.push(q.quote.length);
+        });
+    })
+        .then(function () {
+        let string = QuoteListInts.join(', ');
+        let p = document.createElement('p');
+        p.style.paddingTop = '20px';
+        p.textContent = string.toString();
+        results.appendChild(p);
+    });
+});
+// fourth task
+document.getElementById('fourth-task-button').addEventListener('click', () => {
+    results.innerHTML = '';
+    let givenAuthor = document.getElementById('givenAuthor').value;
+    fetch('./quotes.json')
+        .then(res => res.json())
+        .then(data => {
+        document.getElementById('resultQuoteAmount').value = data.quotes.filter((a) => a.author.toLowerCase().trim() == givenAuthor.toLowerCase().trim()).length;
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Quote } from "./Quote";
 import { QuoteList } from "./QuoteList";
 
 
+let results: HTMLElement = document.getElementById('result-container')!; 
+
 function displayData(list: QuoteList){
 
     let ul = document.createElement('ul')
@@ -19,12 +21,12 @@ function displayData(list: QuoteList){
         li.appendChild(q)
 
         ul.appendChild(li)
-        console.log(quote.id)
     });
     document.getElementById('result-container')!.appendChild(ul)
 
 }
 
+// first task
 document.getElementById('first-task-button')!.addEventListener('click', () => {
     let taskList = {} as QuoteList;
 
@@ -37,3 +39,68 @@ document.getElementById('first-task-button')!.addEventListener('click', () => {
         displayData(taskList)
     })
 })
+
+// second task
+document.getElementById('second-task-button')!.addEventListener('click', () => {
+    document.getElementById('result-container')!.innerHTML = '';
+    let quotesListString: String[] = [];
+
+    fetch('./quotes.json')
+    .then(res => res.json())
+    .then(data => {
+        data.quotes.forEach((e: Quote) => {
+            quotesListString.push(e.quote)
+        });
+    })
+    .then(function (){
+        let ol = document.createElement('ol');
+        results.appendChild(ol);
+        ol.style.paddingTop = '20px'
+    
+        quotesListString.forEach(s => {
+            
+            let li = document.createElement('li')
+            let tempString = s.replace(/\bthe\b/g, '<span style="font-weight:bold;">the</span>')
+            let tempString2 = tempString.replace(/\bThe\b/g, '<span style="font-weight:bold;">The</span>')
+            
+            li.innerHTML = tempString2;
+            ol.appendChild(li);
+        })
+    })
+        
+
+})
+
+// third task
+document.getElementById('third-task-button')!.addEventListener('click', () => {
+    results.innerHTML = '';
+    let QuoteListInts: Number[] = [];
+
+    fetch('./quotes.json')
+    .then(res => res.json())
+    .then(data => {
+        data.quotes.forEach((q: Quote) => {
+            QuoteListInts.push(q.quote.length)
+        });
+    })
+    .then(function(){
+        let string: String = QuoteListInts.join(', ')
+        let p = document.createElement('p')
+        p.style.paddingTop = '20px'
+        p.textContent = string.toString();
+        results.appendChild(p);
+    })
+})
+
+// fourth task
+document.getElementById('fourth-task-button')!.addEventListener('click', () => {
+    results.innerHTML = '';
+
+    let givenAuthor: string = (document.getElementById('givenAuthor') as HTMLInputElement).value; 
+
+    fetch('./quotes.json')
+    .then(res => res.json())
+    .then (data => {
+        (document.getElementById('resultQuoteAmount') as HTMLInputElement).value =  data.quotes.filter((a: Quote) => a.author.toLowerCase().trim() == givenAuthor.toLowerCase().trim()).length;
+    })
+}) 
